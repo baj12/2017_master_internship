c07d0ae2fad2a977e7656dfd98f662ebb7d0bd0d
Savy <lady_son_33@hotmail.fr>
2017-03-29 19:03:52 -0400

    Fixed problem about organism name

--- infoTracker/est_sucking.py
+++ infoTracker/est_sucking.py
@@ -1,8 +1,8 @@
 '''
 Author: Savandara Besse
 
-Created date: 03-28-2017
-Modified date:
+Created date: 03/28/2017
+Modified date: 03/29/2017
 
 Description: This script allows to download all est fasta for each organism
 which choose as HMM for Maker pipeline
@@ -16,27 +16,28 @@ Entrez.email = 'savandara.besse@gmail.com'
 
 def download_EST(pathcsv, estpath):
     df = pd.read_csv(pathcsv)
-    if df['#EST'][0] != 0 :
-        print('Downloading EST: ')
-        search_handle = Entrez.esearch(db = "nucest", term = df['Species name'][0],retmax =df['#EST'][0])
-        id_list = Entrez.read(search_handle)['IdList']
-        print(id_list)
-        search_handle.close()
 
-        out_handle = open(estpath+'/'+df['Species name'][0]+'_est.fna',"w")
-        for elem in id_list :
-            net_handle = Entrez.efetch(db="nucest", id=elem, rettype="fasta", retmode="text")
-            out_handle.write(net_handle.read())
+    for index in df.index :
+        if df['#EST'][index] != 0 :
+            print('Downloading EST: '+ df['Species name'][index])
+            search_handle = Entrez.esearch(db = "nucest", term = df['Species name'][index], field='Organism', retmax =df['#EST'][index])
+            id_list = Entrez.read(search_handle)['IdList']
+            search_handle.close()
+
+            out_handle = open(estpath+'/'+df['Species name'][index]+'_est.fna',"w")
+            for elem in range(len(id_list)) :
+                print('Downloading '+str(elem+1) +'/'+str(len(id_list))+' in progress')
+                net_handle = Entrez.efetch(db="nucest", id=id_list[elem], rettype="fasta", retmode="text")
+                out_handle.write(net_handle.read())
             net_handle.close()
-        out_handle.close()
-        print(df['Species name'][0]+'_est.fna saved')
-
+            out_handle.close()
+            print(df['Species name'][index]+'_est.fna saved')
 
 # Main
 
 parser = OptionParser()
 parser.add_option("-p", "--pathcsv", dest="pathcsv", default="None",
-                  help="[Required] Location of the csv file curated_InsectGenomesInfos.csv")
+                  help="[Required] Location of the csv file hmmOrder.csv")
 parser.add_option("-e", "--estpath", dest="estpath", default="None",
                   help="[Required] Location of the directory where the est will be downloaded")
 
