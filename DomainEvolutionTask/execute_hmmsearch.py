6a69686a842f7401fafb758703de82238dbb5d4b
Savy <lady_son_33@hotmail.fr>
2017-04-08 16:29:20 -0400

    change output extension file

--- DomainEvolutionTask/execute_hmmsearch.py
+++ DomainEvolutionTask/execute_hmmsearch.py
@@ -31,13 +31,16 @@ lotusModel = options.lotusModel
 
 protein_folder = os.listdir(proteinPath)
 
+if not os.path.isdir('oskar_search'):
+    os.mkdir('oskar_search')
+
 bar = progressbar.ProgressBar(widgets=[' [', progressbar.Timer(), '] ', progressbar.Bar(), ' (', progressbar.ETA(), ') ', ])
 for protein in bar(protein_folder) :
-    if not os.path.isdir('{}_oskar_search'.format(protein)):
-    	os.mkdir('{}_oskar_search'.format(protein))
+    if not os.path.isdir('./oskar_search/{}_oskar_search'.format(protein)):
+    	os.mkdir('./oskar_search/{}_oskar_search'.format(protein))
 
     path = os.path.join(proteinPath, protein)
     if os.path.isfile(path) :
         output = re.findall(r'^tsa.([A-Z]{4}.[0-9]{1})',protein)[0]
-        os.system('hmmsearch --tblout ./{}_oskar_search/{}_osk_search.out {} {}'.format(protein,output,oskModel,protein))
-        os.system('hmmsearch --tblout ./{}_oskar_search/{}_lotus_search.out {} {}'.format(protein,output,lotusModel,protein))
+        os.system('hmmsearch --tblout ./oskar_search/{}_oskar_search/{}_osk_search.txt {} {}'.format(protein,output,oskModel,protein))
+        os.system('hmmsearch --tblout ./oskar_search/{}_oskar_search/{}_lotus_search.txt {} {}'.format(protein,output,lotusModel,protein))
